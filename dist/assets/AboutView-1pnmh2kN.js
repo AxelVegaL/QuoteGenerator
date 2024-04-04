46f568d8f1ed35930c4af61ee196973d6404e9b9
Axel Vega <121039412@upq.edu.mx>
2024-04-03 22:56:23 -0600

    Otra prueba deploy

---
+++ dist/assets/AboutView-1pnmh2kN.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,o as t,a as e}from"./index-buGSV3Ws.js";const a={},c={class:"about"},n=e("h1",null,"This is an about page",-1),_=e("a",{class:"text-2xl"}," Creo que era así, no? ",-1),r=[n,_];function i(d,l){return t(),o("div",c,r)}const h=s(a,[["render",i]]);export{h as default};
